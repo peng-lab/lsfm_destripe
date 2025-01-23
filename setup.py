ee8d601ed8cfcf536883615ce3cd6df526c4c1c8
Jianxu Chen <jxchen.ustc@gmail.com>
2025-01-23 09:50:51 +0100

    fix building details

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ from setuptools import find_packages, setup
 with open("README.md") as readme_file:
     readme = readme_file.read()
 
-
 setup_requirements = [
     "pytest-runner>=5.2",
 ]
@@ -71,11 +70,11 @@ setup(
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
     ],
-    description="DeStripe for LSFM in jax",
+    description="DeStripe module in Leonardo toolset for LSFM destriping",
     entry_points={
         "console_scripts": ["destripe=lsfm_destripe.bin.run_destripe:main"],
     },
@@ -88,7 +87,7 @@ setup(
     keywords="lsfm_destripe",
     name="lsfm_destripe",
     packages=find_packages(exclude=["tests", "*.tests", "*.tests.*"]),
-    python_requires=">=3.7",
+    python_requires=">=3.9",
     setup_requires=setup_requirements,
     test_suite="lsfm_destripe/tests",
     tests_require=test_requirements,
